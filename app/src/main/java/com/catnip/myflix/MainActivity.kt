32037a95ccf102718b7f0c5a7725f8d2c0b1368b
Muhammad Hermas Yuda P <hermas.yuda@gmail.com>
2022-08-27 13:46:32 +0700

    Initial commit

---
+++ app/src/main/java/com/catnip/myflix/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.catnip.myflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
